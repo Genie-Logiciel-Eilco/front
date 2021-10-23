ded0c1ef78bdd3eec61ed7f57272dd7c24eda9ff
Iliass Foukhar <nextgendevelopement@gmail.com>
2021-10-23 19:10:51 +0200

    Static Sign up page

---
+++ src/Helpers/API_URL.js
@@ -0,0 +1,3 @@
+const API_ENDPOINT = 'http://104.248.39.111';
+
+export default API_ENDPOINT;
--- src/Pages/Signup/SignupPage.js
+++ src/Pages/Signup/SignupPage.js
@@ -1,13 +1,134 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+// Layout
 import Navbar from '../../Layout/Navbar/Navbar';
 import Footer from '../../Layout/Footer/Footer';
+// Helper
+import formGenerator from '../../Helpers/FormGenerator';
+import { Link } from 'react-router-dom';
+
+// Imgs
+import logo from '../../Assets/templogo.jpg'
+import background from '../../Assets/Login.jpg';
+
+// Styling
+import './SignupPage.scss';
 
 export default function SignupPage() {
+    const [data, setData] = useState({
+        first_name : "",
+        last_name : "",
+        username : "",
+        email : "",
+        password : "",
+        password_confirmation : "",
+        role_id : 2
+    });
+
+    const form = [{
+        label : "Nom",
+        type : "text",
+        name : "last_name",
+        value : data.last_name,
+        onChange : (e) => setData(data => {
+            return {
+                ...data,
+                last_name : e.target.value
+            };
+        })
+    },
+    {
+        label : "Prénom",
+        type : "text",
+        name : "first_name",
+        value : data.first_name,
+        onChange : (e) => setData(data => {
+            return {
+                ...data,
+                first_name : e.target.value
+            };
+        })
+    },
+    {
+        label : "Nom d'utilisateur",
+        type : "text",
+        name : "username",
+        value : data.username,
+        onChange : (e) => setData(data => {
+            return {
+                ...data,
+                username : e.target.value
+            };
+        })
+    },
+    {
+        label : "Email",
+        type : "email",
+        name : "email",
+        value : data.email,
+        onChange : (e) => setData(data => {
+            return {
+                ...data,
+                email : e.target.value
+            };
+        })
+    },
+    {
+        label : "Mot de passe",
+        type : "password",
+        name : "password",
+        value : data.password,
+        onChange : (e) => setData(data => {
+            return {
+                ...data,
+                password : e.target.value
+            };
+        })
+    },
+    {
+        label : "Confirmation du mot de passe",
+        type : "password",
+        name : "password_confirmation",
+        value : data.password_confirmation,
+        onChange : (e) => setData(data => {
+            return {
+                ...data,
+                password_confirmation : e.target.value
+            };
+        })
+    },]
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(data);
+    }
+    
     return (
-        <main>
+        <main className="signup-page-wrapper">
             <Navbar />
-                
+            <img src={background} className="login-page-bg" />
+
+            <div className="signup-form-wrapper">
+                <form onSubmit={handleSubmit}>    
+                    <div className="logo-login-wrapper">
+                            <img src={logo} className="logo-login" />
+                        </div>
+                        <legend>
+                            On est très content de vous avoir  <br />parmis nous !
+                        </legend>
+                        {formGenerator(form)}
+                        <button type="submit" className="btn-login" >
+                            S'enregistrer
+                        </button>
+
+                        <Link to="/Signup"> 
+                        <button type="button" role="button" className="btn-ternary-login">
+                            Vous avez déja un compte ?
+                            </button>
+                        </Link>
+                        
+                    </form>
+                </div>
             <Footer />
         </main>
     )
