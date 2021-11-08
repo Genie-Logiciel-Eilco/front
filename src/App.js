1a25d411e2e1ef4084957371acb3e0d8e6e6db4e
aymn <aymane.elammar@gmail.com>
2021-11-08 11:39:19 +0100

    b

--- src/App.js
+++ src/App.js
@@ -36,10 +36,10 @@ function App() {
           <Route path="/books" exact component={Browsepage} />
           <Route path="/books/:id" exact component={Bookpage} />
           <Route exact path="/69420/home" component={dashHome} />
-          {/* <Route exact path="/69420/users" component={Users} />
+          <Route exact path="/69420/users" component={Users} />
           <Route exact path="/69420/books/book" component={Books} />
           <Route exact path="/69420/books/author" component={Authors} />
-          <Route exact path="/69420/books/publisher" component={Publishers} /> */}
+          <Route exact path="/69420/books/publisher" component={Publishers} />
           
 
         </Switch>
--- src/Pages/Signup/SignupPage.js
+++ src/Pages/Signup/SignupPage.js
@@ -24,7 +24,6 @@ export default function SignupPage() {
         email: "",
         password: "",
         password_confirmation: "",
-        role_id: 2,
     });
 
     const form = [
